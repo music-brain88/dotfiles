2596a558b7ad2acc2a9b698c6138abaf473431c4
music-brain88 <music.brain88@gmail.com>
2019-09-28 20:38:03 +0900

    Modified bash script

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 set -u
 
 # 実行場所のディレクトリを取得
-THIS_DIR=$(cd $(dirname $0); pwd)
+#THIS_DIR=$(cd $(dirname $0); pwd)
 
-cd $THIS_DIR
+#cd $THIS_DIR
 
 echo "start setup..."
 
@@ -19,7 +19,7 @@ touch init.d
 ln -snfv ~/dotfiles/.bashrc ~/.bashrc
 ln -snfv ~/dotfiles/nvim/init.d ~/.config/nvim/init.d
 ln -snfv ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -snfv ~/mac_dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -snfv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 echo "finish setup"
 echo "next you call dein script"
