b2820b28dbc241e63d482b78d5f6f3585a1ef7e3
music-brain88 <music.brain88@gmail.com>
2019-09-28 22:01:48 +0900

    remove setup.sh install.sh rename to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+set -u
 
-    echo "$f"
-done
+echo "start setup..."
+
+# For example, we just use `~/.cache/dein` as installation directory
+if [!-d ~/.cache/dein]; then
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+  sh ./installer.sh ~/.cache/dein
+  rm installer.sh
+fi
+
+mkdir -p ~/.config/nvim/
+
+cd ~/.config/nvim/
+
+touch init.vim
+touch init.d
+
+ln -snfv ~/dotfiles/.bashrc ~/.bashrc
+ln -snfv ~/dotfiles/nvim/init.d ~/.config/nvim/init.d
+ln -snfv ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -snfv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+echo "finish setup"
+echo "next you call dein script"
