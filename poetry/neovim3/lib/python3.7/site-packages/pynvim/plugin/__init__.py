7ea4187e0e8180736076a03d387e433f0cc8f4e3
music-brain88 <music.brain88@gmail.com>
2020-01-08 23:10:46 +0900

    remove poetry folder

--- poetry/neovim3/lib/python3.7/site-packages/pynvim/plugin/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Nvim plugin/host subpackage."""
-
-from .decorators import (autocmd, command, decode, encoding, function,
-                         plugin, rpc_export, shutdown_hook)
-from .host import Host
-
-
-__all__ = ('Host', 'plugin', 'rpc_export', 'command', 'autocmd',
-           'function', 'encoding', 'decode', 'shutdown_hook')
