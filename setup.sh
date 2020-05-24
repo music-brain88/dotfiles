da65307e01c098a4cb37a9d9207d15153adcd2ff
music-brain88 <music.brain88@gmail.com>
2020-05-24 15:35:04 +0900

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,14 @@ set -u
 echo "start setup..."
 
 # For example, we just use `~/.cache/dein` as installation directory
-if [!-d ~/.cache/dein]; then
+if [ ! -d ~/.cache/dein ]; then
+  echo "Dein is not exists"
+  echo "Download Dein scripts"
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
   sh ./installer.sh ~/.cache/dein
   rm installer.sh
+else
+  echo "Dein is exists, Skip Download"
 fi
 
 mkdir -p ~/.config/nvim/
@@ -16,7 +20,7 @@ mkdir -p ~/.config/nvim/
 touch ~/.config/nvim/init.vim
 
 ln -snfv ~/dotfiles/.bashrc ~/.bashrc
-ln -snfv ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -snfv ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 
 # bash_profileが整理されてないので一旦退避
 if [ "$(uname)" == 'Darwin' ]; then
@@ -25,18 +29,22 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   ln -snfv ~/dotfiles/.bash_profile ~/.bash_profile
 fi
 
-ln -snfv ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -snfv ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -snfv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
-echo "install Rust compiler"
-curl https://sh.rustup.rs -sSf | sh
+if !(type rustup > /dev/null 2>&1); then
+  echo "install Rust compiler"
+  curl https://sh.rustup.rs -sSf | sh
+else
+  echo "Rust is Installed"
+fi
 
 # fish settings
-ln -snfv ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -snfv ~/dotfiles/fish/completions/fisher.fish ~/.config/fish/completions/fisher.fish
-ln -snfv ~/dotfiles/fish/completions/git.fish ~/.config/fish/completions/git.fish
-ln -snfv ~/dotfiles/fish/completions/pyenv.fish ~/.config/fish/completions/pyenv.fish
-ln -snfv ~/dotfiles/fish/completions/poetry.fish ~/.config/fish/completions/poetry.fish
+ln -snfv ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
+ln -snfv ~/dotfiles/.config/fish/completions/fisher.fish ~/.config/fish/completions/fisher.fish
+ln -snfv ~/dotfiles/.config/fish/completions/git.fish ~/.config/fish/completions/git.fish
+ln -snfv ~/dotfiles/.config/fish/completions/pyenv.fish ~/.config/fish/completions/pyenv.fish
+ln -snfv ~/dotfiles/.config/fish/completions/poetry.fish ~/.config/fish/completions/poetry.fish
 
 echo "finish setup"
 echo "next you call dein script"
