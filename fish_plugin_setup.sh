7f827e3e2cc9df33fb684f15724a6cdc1984e314
music-brain88 <music.brain88@gmail.com>
2020-07-12 17:19:47 +0900

    remake fish plugin script

---
+++ fish_plugin_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ue
+
+
+fisher add oh-my-fish/theme-bobthefish
+fisher add jethrokuan/fzf
+fisher add oh-my-fish/plugin-extract
+fisher add jorgebucaran/fish-bax
+fisher add jethrokuan/z
+fisher add edc/bass
