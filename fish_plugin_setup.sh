02ef60b6ab276a2c3ae08e38d7bb677aeb407100
music-brain88 <music.brain88@gmail.com>
2020-07-14 13:09:38 +0900

    remove sh

--- fish_plugin_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ue
-
-
-fisher add oh-my-fish/theme-bobthefish
-fisher add jethrokuan/fzf
-fisher add oh-my-fish/plugin-extract
-fisher add jorgebucaran/fish-bax
-fisher add jethrokuan/z
-fisher add edc/bass
