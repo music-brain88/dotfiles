df73170c7a27fd7ecd387d20372f525a513ab75b
music-brain88 <music.brain88@gmail.com>
2021-06-27 03:02:31 +0900

    Add install.sh

---
+++ .bin/install.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+DOTFILES="$(pwd)"
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
+
+backup() {
+    BACKUP_DIR=$HOME/dotfiles-backup
+
+    echo "Creating backup directory at $BACKUP_DIR"
+    mkdir -p "$BACKUP_DIR"
+
+    for file in $(get_linkables); do
+        filename=".$(basename "$file" '.symlink')"
+        target="$HOME/$filename"
+        if [ -f "$target" ]; then
+            echo "backing up $filename"
+            cp "$target" "$BACKUP_DIR"
+        else
+            warning "$filename does not exist at this location or is a symlink"
+        fi
+    done
+
+    for filename in "$HOME/.config/nvim" "$HOME/.vim" "$HOME/.vimrc"; do
+        if [ ! -L "$filename" ]; then
+            echo "backing up $filename"
+            cp -rf "$filename" "$BACKUP_DIR"
+        else
+            warning "$filename does not exist at this location or is a symlink"
+        fi
+    done
+}
+
+case "$1" in
+    backup)
+        backup
+        ;;
+    link)
+        setup_symlinks
+        ;;
+    git)
+        setup_git
+        ;;
+    homebrew)
+        setup_homebrew
+        ;;
+    shell)
+        setup_shell
+        ;;
+    terminfo)
+        setup_terminfo
+        ;;
+    macos)
+        setup_macos
+        ;;
+    all)
+        setup_symlinks
+        setup_terminfo
+        setup_homebrew
+        setup_shell
+        setup_git
+        setup_macos
+        ;;
+    *)
+        echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
+        exit 1
+        ;;
+esac
+
+echo -e
+success "Done."
