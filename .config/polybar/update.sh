803843eb8d72a521b347e3915b761264e9e23eae
music-brain88 <music.brain88@gmail.com>
2021-07-09 00:34:38 +0900

    remove radius, add font another size

--- .config/polybar/update.sh
+++ .config/polybar/update.sh
@@ -1,19 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+echo
+echo 'updates.sh: "paru -Syu"'
+echo
+yay -Syu
 
-if ! updates_aur=$(paru -Qua 2> /dev/null | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
+echo
+bash ~/.config/polybar/checkupdates.sh
 
-updates=$(("$updates_arch" + "$updates_aur"))
-
-if [ "$updates" -gt 0 ]; then
-    echo "  $updates"
-else
-    echo "  $updates"
-fi
+read -p "Press enter to close this window..."
