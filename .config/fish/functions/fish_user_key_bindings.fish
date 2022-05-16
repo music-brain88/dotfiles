8a55f3e5feade798f940e60fd3b00f17efb87642
music-brain88 <music.brain88@gmail.com>
2022-05-17 05:28:30 +0900

    Change key bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,7 @@
 function fish_user_key_bindings
-  bind \cd fzf-docker-continer-name-select
+  # Ctrl + y を潰す理由 
+  # 貼り付け。ctrl+wなどで一気に削除された部分を貼り付けられる。
+  # マウスやtmuxのコピーモードで代用できるので潰す優先度は高い。
+  bind \cy fzf-docker-continer-name-select
   fzf_key_bindings
 end
