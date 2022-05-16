edcf722c44f3f9c487237e94dc0774cf22f5ab6d
music-brain88 <music.brain88@gmail.com>
2022-05-17 05:12:02 +0900

    add new git command alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,6 +50,17 @@ export TERM=xterm-256color
 # set startship
 #starship init fish | source
 
+function fzf-checkout-branch
+    set -l branchname (
+        env FZF_DEFAULT_COMMAND='git --no-pager branch -a | grep -v HEAD | sed -e "s/^.* //g"' \
+            fzf --height 70% --prompt "BRANCH NAME>" \
+                --preview "git --no-pager log -20 --color=always {}"
+    )
+    if test -n "$branchname"
+        git checkout (echo "$branchname"| sed "s#remotes/[^/]*/##")
+    end
+end
+
 function reload
   exec fish
 end
