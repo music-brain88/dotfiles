d1c20d8cbb84e14c91c46faf62953a6480faac6c
music-brain88 <music.brain88@gmail.com>
2024-04-01 22:48:51 +0900

    Add key-binddings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -45,10 +45,10 @@ set __fish_git_prompt_color_branch yellow
 
 
 # set fzf setting
-set -x PATH ~/.fzf/bin $PATH
-set -x FZF_LEGACY_KEYBINDINGS 0
-set -x FZF_REVERSE_ISEARCH_OPTS "--height=50%"
-set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+# set -x PATH ~/.fzf/bin $PATH
+# set -x FZF_LEGACY_KEYBINDINGS 0
+# set -x FZF_REVERSE_ISEARCH_OPTS "--height=50%"
+# set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
 
 # set GO PATH
 set -x GOPATH $HOME/go
