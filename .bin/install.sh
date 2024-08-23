ef489bb8f878d56c360601d9301e367d353e4a59
music-brain88 <music.brain88@gmail.com>
2024-08-23 23:16:43 +0900

    mv

--- .bin/install.sh
+++ .bin/install.sh
@@ -8,6 +8,17 @@ UTILS_DIR="$SCRIPT_DIR/utils"
 
 echo "Starting dotfiles installation..."
 
+# 必要なディレクトリを作成
+# TODO: この部分をもっとスマートに書く
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/fish
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/starship
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/mpd
+mkdir -p ~/.config/ncmpcpp
 
 # Run setup scripts
 # install.sh スクリプト内で、source コマンドの前に ShellCheck の指示コメントを追加する。
