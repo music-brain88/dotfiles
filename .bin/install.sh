5e7445ce107c264816ca189e6d17047e94a9ef0e
music-brain88 <music.brain88@gmail.com>
2024-08-23 22:15:53 +0900

    Add comment for shellcheck

--- .bin/install.sh
+++ .bin/install.sh
@@ -10,12 +10,20 @@ echo "Starting dotfiles installation..."
 
 
 # Run setup scripts
+# install.sh スクリプト内で、source コマンドの前に ShellCheck の指示コメントを追加する。
+# shellcheck source=./utils/setup_base.sh
 source "$UTILS_DIR/setup_base.sh"
+# shellcheck source=./utils/setup_symlinks.sh
 source "$UTILS_DIR/setup_symlinks.sh"
+# shellcheck source=./utils/setup_neovim.sh
 source "$UTILS_DIR/setup_neovim.sh"
+# shellcheck source=./utils/setup_rust_tools.sh
 source "$UTILS_DIR/setup_rust_tools.sh"
+# shellcheck source=./utils/setup_terminal.sh
 source "$UTILS_DIR/setup_terminal.sh"
+# shellcheck source=./utils/setup_shell.sh
 source "$UTILS_DIR/setup_shell.sh"
+# shellcheck source=./utils/setup_tmux.sh
 source "$UTILS_DIR/setup_tmux.sh"
 
 echo "Dotfiles installation complete!"
