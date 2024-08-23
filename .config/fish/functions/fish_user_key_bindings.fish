49c3647860de2501447690c926d2dab1bac0c0b0
music-brain88 <music.brain88@gmail.com>
2024-08-23 14:46:59 +0900

    replace fzf to skim

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
   # Ctrl + y を潰す理由 
   # 貼り付け。ctrl+wなどで一気に削除された部分を貼り付けられる。
   # マウスやtmuxのコピーモードで代用できるので潰す優先度は高い。
-  bind \cy fzf-docker-continer-name-select
+  bind \cy skim-docker-container-name-select
 
 
     #!/bin/fish
