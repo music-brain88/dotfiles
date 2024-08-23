080a40ee43cf5fc597d673b9c85bb4a75a04899d
1saver <43394582+music-brain88@users.noreply.github.com>
2024-08-23 15:13:45 +0900

    feat(fish, neovim): fix deprecated, replace fzf (#156)
    ## [optional body]
    - fzf replace to skim
    - modified ddc setting
    ## [optional footer(s)]

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 alias vim='nvim'
-# set PATH $TO_FISH_PATH $PATH
 
 # set pyenv path
 set -x PYENV_ROOT $HOME/.pyenv
@@ -22,7 +21,7 @@ if type -q test eza
 end
 
 # set bat alias
-if type -q bat 
+if type -q bat
   alias cat='bat'
 end
 
@@ -31,12 +30,10 @@ if type -q procs
   alias ps='procs'
 end
 
-# set poetry path
-# set -U PATH $HOME/.poetry/env $PATH
-
-# if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval $(rbenv init - | source)
+# set jql alias
+#if type -q jql
+#  alias jq='jql'
+#end
 
 # fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
@@ -44,38 +41,40 @@ set __fish_git_prompt_showstashstate 'yes' set __fish_git_prompt_showupstream 'y
 set __fish_git_prompt_color_branch yellow
 
 
-# set fzf setting
-set -x PATH ~/.fzf/bin $PATH
-set -x FZF_LEGACY_KEYBINDINGS 0
-set -x FZF_REVERSE_ISEARCH_OPTS "--height=50%"
-set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
-
 # set GO PATH
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 
 export TERM=xterm-256color
 
-function fzf-checkout-branch
+# set skim setting
+set -x PATH ~/.skim/bin $PATH
+set -x SKIM_DEFAULT_COMMAND 'rg --files --hidden --follow --glob "!.git/*"'
+
+function skim-checkout-branch
     set -l branchname (
-        env FZF_DEFAULT_COMMAND='git --no-pager branch -a | grep -v HEAD | sed -e "s/^.* //g"' \
-            fzf --height 70% --prompt "BRANCH NAME>" \
+        env SKIM_DEFAULT_COMMAND='git --no-pager branch -a | grep -v HEAD | sed -e "s/^.* //g"' \
+            sk --height 70% --prompt "BRANCH NAME>" \
                 --preview "git --no-pager log -20 --color=always {}"
     )
+
     if test -n "$branchname"
         git checkout (echo "$branchname"| sed "s#remotes/[^/]*/##")
     end
 end
 
-function fzf-docker-continer-name-select
-    commandline -i (env FZF_DEFAULT_COMMAND="docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Networks}}'" \
-        fzf --no-sort --height 80% --bind='p:toggle-preview' --preview-window=down:70% \
+
+function skim-docker-container-name-select
+    commandline -i (env SKIM_DEFAULT_COMMAND="docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.Command}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Networks}}'" \
+        sk --no-sort --height 80% --bind='p:toggle-preview' --preview-window=down:70% \
             --preview '
                 set -l containername (echo {} | awk -F " " \'{print $2}\');
                 if test "$containername" != "ID"
+
                     docker logs --tail 300 $containername
                 end
             ' | \
+
         awk -F " " '{print $2}')
 end
 
